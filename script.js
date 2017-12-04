d678c361340c37ca936c918429c00b6944f85f1d
kwolfski <kt.schramm.design@gmail.com>
2017-12-03 22:02:18 -0600

    process

--- script.js
+++ script.js
@@ -1,42 +1,7 @@
 // Slide Reveal 
 
-//Alabama
 $(document).ready(function(){
-    $("#alabama").click(function(){
-        $("#Montgomery").slideToggle("slow");
+    $("#state").click(function(){
+        $("#answer").slideToggle("slow");
     });
 });
-
-
-//Alaska
-$(document).ready(function(){
-    $("#alaska").click(function(){
-        $("#Juneau").slideToggle("slow");
-    });
-});
-
-
-//Arkansas
-$(document).ready(function(){
-    $("#arkansas").click(function(){
-        $("#LittleRock").slideToggle("slow");
-    });
-});
-
-
-//California
-$(document).ready(function(){
-    $("#california").click(function(){
-        $("#Sacramento").slideToggle("slow");
-    });
-});
-
-
-//Colorado
-$(document).ready(function(){
-    $("#colorado").click(function(){
-        $("#Denver").slideToggle("slow");
-    });
-});
-
-
